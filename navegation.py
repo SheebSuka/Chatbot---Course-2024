fd399019f0a2ce2d4f9742d43cd2e3ef945c2e7f
Sheeb <thingisss89@gmail.com>
2024-10-23 16:32:28 -0700

    Back Method

--- navegation.py
+++ navegation.py
@@ -5,8 +5,21 @@ driver = webdriver.Chrome()
 driver.set_window_position(0,0)
 driver.set_window_size(1024,720)
 
+#Navegacion hacia pagina de Whatsapp
+driver.get("https://web.whatsapp.com/")
+print(driver.title)
+time.sleep(15)
+
+
 # Navegacion hacia un URL
 driver.get("https://www.google.com")
 print(driver.title)
-time.sleep(25)
+time.sleep(15)
+
+#Back METHOD
+driver.back()
+print(driver.current_url)
+time.sleep(5)
+
+
 
