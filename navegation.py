970e4065833e0b7214388047d2f1c579099b74dd
Sheeb <thingisss89@gmail.com>
2024-10-23 16:37:19 -0700

    Fowward Method

--- navegation.py
+++ navegation.py
@@ -8,18 +8,22 @@ driver.set_window_size(1024,720)
 #Navegacion hacia pagina de Whatsapp
 driver.get("https://web.whatsapp.com/")
 print(driver.title)
-time.sleep(15)
+time.sleep(5)
 
 
 # Navegacion hacia un URL
 driver.get("https://www.google.com")
 print(driver.title)
-time.sleep(15)
+time.sleep(5)
 
 #Back METHOD
 driver.back()
 print(driver.current_url)
 time.sleep(5)
 
+#Foward Method
+driver.forward()
+print(driver.current_url)
+time.sleep(5)
 
 
