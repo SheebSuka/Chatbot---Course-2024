da9c70af0768973e9a14baa1419e198a76c6bfb6
Sheeb <thingisss89@gmail.com>
2024-10-24 12:50:49 -0700

    Station dicctionari

--- estacionesMeterologicas.py
+++ estacionesMeterologicas.py
@@ -28,3 +28,16 @@ station_row = {
     'Precipitation':'1',
     'Radiation':'1'
 }
+
+#Se declaran variables a las que asignaremos al diccionario asociando al valor po rmedio de la llave.
+station_id = station_row['ID']
+station_name = station_row['Station Name']
+station_elev-m = station_row['Elev-m']
+station_lat = station_row['Lat']
+station_lon = station_row['Lon']
+station_type = station_row['Type']
+station_temp = station_row['Temperature']
+station_at_pre = station_row['Atmospheric_Pressure']
+station_humidity = station_row['Humidity']
+station_precipitation = station_row['Precipitation']
+station_radiation = station_row['Radiation']
