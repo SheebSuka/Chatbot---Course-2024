9a888ac35485b4e35f33d91e8be12ae43625332b
Sheeb <thingisss89@gmail.com>
2024-10-30 13:37:43 -0700

    Se sacaron datos del Xpath desde google chrome

--- prueba.py
+++ prueba.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 import time
 
+
 # Ejecutamos las opciones
 options = webdriver.ChromeOptions()
 options.add_argument(
@@ -19,3 +20,5 @@ time.sleep(3600)
 
 # //*[@id="pane-side"]/descendant::span[contains(@aria-label,'Unread')]
 # //*[@id="main"]/descendant::div[@role='row']
+#//div/div/div[1]/div[1]/div[1]/div/div[1]/div/span[1]/span
+#//*[@id="main"]/footer/div[1]/div/span/div/div[2]/div[1]/div/div[1]
